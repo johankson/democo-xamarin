90ba6023b6989c8f7cd0dc03de78dafa14ae5a45
Johan Karlsson <johan.kson@gmail.com>
2017-04-03 07:54:30 +0200

    Added View and ViewModel folders, fody and ACR.UserDialogs

---
+++ src/Demo/Demo/ViewModels/ViewModelBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Acr.UserDialogs;
+using PropertyChanged;
+using Xamarin.Forms;
+
+namespace ViewModels
+{
+	[ImplementPropertyChanged]
+	public abstract class ViewModelBase
+	{
+		private INavigation _navigation;
+
+		public ViewModelBase()
+		{
+		}
+
+		public INavigation Navigation
+		{
+			get
+			{
+				if (_navigation == null)
+				{
+					throw new Exception("Navigation is not assigned, make sure you assign it before passing the ViewModel to the BindingContext of the view.");
+				}
+				return _navigation;
+			}
+			set
+			{
+				_navigation = value;
+			}
+		}
+
+		public bool IsBusy { get; set; }
+
+		public void SetBusy()
+		{
+			IsBusy = true;
+		}
+
+		public void SetReady()
+		{
+			IsBusy = false;
+		}
+
+		public static IProgressDialog Busy(String text)
+		{
+			return UserDialogs.Instance.Loading(text, maskType: MaskType.Black);
+		}
+	}
+}
